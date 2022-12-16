a59525f76f6b7a78e6e2d8279e9f7e156aa29bb1
Fellipe Matos <45209404+fellipematos@users.noreply.github.com>
2022-12-16 10:58:34 -0300

    Update

--- app.py
+++ app.py
@@ -2,27 +2,30 @@ import streamlit as st
 import httpx
 import time
 
-
+#layout da pagina
 col1, col2 = st.columns(2)
 
 def getCep(cep):
+    #consulta api
     api = "https://brasilapi.com.br/api/cep/v2/"
     result = httpx.get(api + cep)
     return result
 
 def resultCep(cep):
+    #processa o resultado
     with col2:
         action = getCep(cep)
-        with st.spinner(text='processando ...'):
+        with st.spinner(text='buscando cep ...'):
             time.sleep(1)
             if action.status_code == 200:
                     st.success('Concluido')
             elif action.status_code == 404:
                     st.error('!Erro - CEP não encontrado')
 
-        st.write(action)
-        st.write(action.json())
+        st.write(action)#print response
+        st.write(action.json())#print informacoes
 
+#formulario
 with col1:
     with st.form(key='form'):
         cep = st.text_input('DIGITE O CEP:')
